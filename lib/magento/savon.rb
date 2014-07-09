889f8c768aea4f9086593ffe0b8d06ab69859dc7
sanjay <sgupta@enbake.com>
2014-07-09 17:19:57 +0530

    Update savon.rb

--- lib/magento/savon.rb
+++ lib/magento/savon.rb
@@ -5,7 +5,7 @@
 require 'savon'
  
 Savon.configure do |config|
-  config.log = false            # disable logging
+  config.log = true            # disable logging
   #config.log_level = :info      # changing the log level
   #config.logger = Rails.logger  # using the Rails logger
 end
